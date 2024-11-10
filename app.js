4e19a3f8689e459d5118b85e2e5acaf4d41114f8
Arguera123 <00090822@uca.edu.sv>
2024-11-10 00:04:37 -0600

    Iniciando Repo de Mierda 2

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import router from './routers/index.router.js';
 import { connect } from './config/database.config.js'; 
 
+const setCorsHeaders = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite todas las fuentes
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeceras permitidas
+};
+
 // Config server
 const app = (req, res) => {
   try {
+    setCorsHeaders(res);
+    
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200);
+      res.end();
+      return;
+    }
+
     router.handle(req, res);
   } catch (error) {
-    
+    router.handleError(error, req, res); 
   }
 };
 
